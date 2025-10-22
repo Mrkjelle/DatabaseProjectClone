d21e050789128f4a93d3ca1253c27111b342031b
Mrkjelle <hu6613kj-s@student.lu.se>
2025-10-22 16:50:20 +0200

    add configservice

---
+++ DatabaseClient/Data/ConfigService.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+
+namespace DatabaseClient.Data
+{
+    public static class ConfigService
+    {
+        private static IConfigurationRoot? _config;
+
+        public static void Initialize()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        public static string GetConnection(string name)
+        {
+            if (_config == null)
+            {
+                Initialize();
+            }
+
+            return _config!.GetConnectionString(name)
+            ?? throw new InvalidOperationException($"Connection string '{name}' not found.");
+        }
+    }
+}

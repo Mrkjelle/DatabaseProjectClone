b39dc568d1abf52622fb2e2570dfddce84699fb4
Mrkjelle <hu6613kj-s@student.lu.se>
2025-10-23 13:10:38 +0200

    feat: Add Division class for managing division data

---
+++ DatabaseClient/Models/Division.cs
@@ -0,0 +1,20 @@
+namespace DatabaseClient.Models
+{
+    public class Division
+    {
+        public int DivisionID { get; set; }
+        public string DivisionCode { get; set; } = string.Empty;
+        public string DivisionName { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+
+        public Division() { }
+
+        public Division(int divisionID, string divisionCode, string divisionName, string location)
+        {
+            DivisionID = divisionID;
+            DivisionCode = divisionCode;
+            DivisionName = divisionName;
+            Location = location;
+        }
+    }
+}

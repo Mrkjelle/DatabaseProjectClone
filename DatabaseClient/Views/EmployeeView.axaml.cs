2199283a87e30b18ce438a47e79b800902277ec7
Mrkjelle <hu6613kj-s@student.lu.se>
2025-10-25 16:50:23 +0200

    feat: Implement EmployeeView and EmployeeViewModel for employee directory display

---
+++ DatabaseClient/Views/EmployeeView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using DatabaseClient.ViewModels;
+
+namespace DatabaseClient.Views
+{
+    public partial class EmployeeView : UserControl
+    {
+        public EmployeeView()
+        {
+            InitializeComponent();
+            DataContext = new ViewModels
+        }
+    }
+}

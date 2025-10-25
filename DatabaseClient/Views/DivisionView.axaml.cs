ff545415131184b07cbfdb77bcce4dff19430b67
Mrkjelle <hu6613kj-s@student.lu.se>
2025-10-25 22:02:07 +0200

    feat: Implement sorting for divisions in DivisionView on visual tree attachment

--- DatabaseClient/Views/DivisionView.axaml.cs
+++ DatabaseClient/Views/DivisionView.axaml.cs
@@ -1,4 +1,9 @@
 using Avalonia.Controls;
+using System.Linq;
+using Avalonia.Interactivity;
+using DatabaseClient.ViewModels;
+using DatabaseClient.Models.Org;
+using Avalonia;
 
 namespace DatabaseClient.Views;
 
@@ -8,5 +13,18 @@ public partial class DivisionView : UserControl
     {
         InitializeComponent();
         DataContext = new ViewModels.DivisionViewModel();
+    }
+
+    private void OnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        if (DataContext is ViewModels.DivisionViewModel vm)
+        {
+            var sorted = vm.Divisions.OrderBy(div => div.DivisionCode).ToList();
+            vm.Divisions.Clear();
+            foreach (var div in sorted)
+            {
+                vm.Divisions.Add(div);
+            }
+        }
     }
 }
